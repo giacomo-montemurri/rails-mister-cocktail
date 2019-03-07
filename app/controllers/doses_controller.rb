bd86860cedf37f382a5ef81a127e15b945c44b08
giacomo montemurri <gia94.montemurri@yahoo.it>
2019-03-07 18:18:51 +0100

    rails-mister-cocktail

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -9,9 +9,13 @@ class DosesController < ApplicationController
   def create
     @dose = Dose.new(dose_params)
     @dose.cocktail = Cocktail.find(params[:cocktail_id])
-    @dose.save!
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    if @dose.save
+      redirect_to cocktail_path(@dose.cocktail)
+    else
+      render :new
+    end
 
-    redirect_to cocktail_path(@dose.cocktail)
   end
 
   def destroy
@@ -24,6 +28,6 @@ class DosesController < ApplicationController
   private
 
   def dose_params
-  params.require(:dose).permit(:description, :ingredient_id)
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
   end
 end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,9 +1,18 @@
+# class Dose < ApplicationRecord
+#   belongs_to :cocktail
+#   belongs_to :ingredient
+
+#   validates :description, presence: true
+#   # validates :cocktail_id, uniqueness: true, presence: true
+#   # validates :ingredient_id, uniqueness: true, presence: true
+#   validates_uniqueness_of :cocktail, :scope => [:ingredient]
+# end
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
 
   validates :description, presence: true
-  # validates :cocktail_id, uniqueness: true, presence: true
-  # validates :ingredient_id, uniqueness: true, presence: true
-  validates_uniqueness_of :cocktail, :scope => [:ingredient]
+  validates :cocktail, presence: true
+  validates :ingredient, presence: true
+  validates :ingredient_id, uniqueness: { scope: :cocktail_id }
 end
